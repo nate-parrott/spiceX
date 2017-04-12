680ae7250c095423c85c40212c1e919f9909fd12
= <nateparro2t@gmail.com>
2017-04-12 17:34:05 -0400

    add google aopi

--- webapp/recognition.py
+++ webapp/recognition.py
@@ -5,31 +5,64 @@ import json
 
 secrets = json.load(open('secrets.json'))
 
-class Recognize(webapp2.RequestHandler):
-    def post(self):
-        data = self.request.body
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(secrets['clarifai_token'])
+def recognize_clarifai(data):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(secrets['clarifai_token'])
+    }
+    req = {
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "base64": b64encode(data)
+            }
+          }
         }
-        req = {
-          "inputs": [
+      ]
+    }
+    url = 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs'
+    api_resp = json.loads(post(url, data=json.dumps(req), headers=headers).text)
+    
+    concepts = api_resp['outputs'][0]['data']['concepts']
+    foods = [(c['name'], c['value']) for c in concepts]
+    response = {
+        "foods": foods
+    }
+
+def recognize_google(data):
+    key = secrets['google_key']
+    url = "https://vision.googleapis.com/v1/images:annotate?key=" + key
+    req = {
+      "requests": [
+        {
+          "image": {
+            "content": b64encode(data)
+          },
+          "features": [
             {
-              "data": {
-                "image": {
-                  "base64": b64encode(data)
-                }
-              }
+              "type": "LABEL_DETECTION"
             }
           ]
         }
-        url = 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs'
-        api_resp = json.loads(post(url, data=json.dumps(req), headers=headers).text)
-        
-        concepts = api_resp['outputs'][0]['data']['concepts']
-        foods = [(c['name'], c['value']) for c in concepts]
-        response = {
-            "foods": foods
-        }
-        
+      ]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    resp = json.loads(post(url, data=json.dumps(req), headers=headers).text)
+    if 'response' not in resp:
+        print 'ERROR:'
+        print resp
+    labels = resp['responses'][0]['labelAnnotations']
+    foods = [[l['description'], l['score']] for l in labels]
+    return {
+        "foods": foods
+    }
+    
+
+class Recognize(webapp2.RequestHandler):
+    def post(self):
+        data = self.request.body
+        response = recognize_google(data)
         self.response.write(json.dumps(response))
