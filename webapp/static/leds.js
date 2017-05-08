b37ffe06e48f805144cadba7898baaed07b68ab7
= <nateparro2t@gmail.com>
2017-05-07 23:57:48 -0400

    xyz

--- webapp/static/leds.js
+++ webapp/static/leds.js
@@ -3,7 +3,7 @@
 function setLeds(pins) {
   //console.log('Setting LEDs:', pins);
   let url = 'http://localhost:8999/leds?pins=' + pins.join(',');
-  //$.post(url, {}, (resp) => {});
+  $.post(url, {}, (resp) => {});
 }
 
 // setLeds([2,3])
