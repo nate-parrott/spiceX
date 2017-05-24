82c3a0b913f2bf462dbb440a9bdd7d4d144df844
= <nateparro2t@gmail.com>
2017-05-24 18:07:35 -0400

    work

--- util/imagesizer.py
+++ util/imagesizer.py
@@ -12,6 +12,7 @@ while True:
     x = raw_input(' [image names separated by spaces] ')
     if x == '': break
     names = x.split()
+    names = [(name + '.png' if '.' not in name else name) for name in names]
     paths = [os.path.join('images', name) for name in names]
     sizes = [get_size(path) for path in paths]
     container_aspect = sum(w/h for (w,h) in sizes)
@@ -20,7 +21,7 @@ while True:
     output += "<div class='multi' style='padding-bottom: {}'><ul>".format(percent(1.0/container_aspect))
     for path, (w,h) in zip(paths, sizes):
         aspect = w/h
-        output += "<li style='width: {}'><img src='{}' /></li>".format(percent(aspect/container_aspect*0.95), path)
+        output += "<li style='flex-grow: {}'><img src='{}' /></li>".format(aspect/container_aspect, path)
     output += "</ul></div>"
     print '\n' + output + '\n'
     
